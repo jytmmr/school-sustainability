ee2dbb7dc0438eb9ddf2a4ccc1d13e16d7453425
jay <j.timmer11@yahoo.com>
2018-01-06 16:48:36 -0600

    Add seeding for user database table

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,8 +17,14 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
     return [
         'name' => $faker->name,
+        'school'=> $faker->colorName . " School",
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'api_token' => str_random(10),
     ];
+});
+
+$factory->define(App\TeachingData::class, function (Faker\Generator $faker) {
+    return [];
 });
